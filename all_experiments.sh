4d64aa99bc32038a609eafcab669f40b1998eaf0
brobeson <brobeson@users.noreply.github.com>
2022-02-04 23:29:04 -0700

    Remove the pilot study from all experiments
    I also removed the report line.

--- all_experiments.sh
+++ all_experiments.sh
@@ -6,11 +6,6 @@ then
   exit 1
 fi
 
-# Pilot Study
-./tmft.py pilot \
-  --tracker-name "${1}" \
-  Car4 Car24 Deer FleetFace Jump
-  
 # VOT 2019
 ./tmft.py experiment \
   --tracker-name "${1}" \
@@ -31,6 +26,3 @@ fi
   --dataset-dir ~/Videos/uav123 \
   --slack-file ~/.slack_channel.yml \
   uav123
-
-# Reports
-./tmft.py report
